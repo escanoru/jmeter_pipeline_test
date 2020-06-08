f6343c22e902eda54c379a0f0c65cb1abe0efd5e
escanoru <yitzo@yandex.com>
2020-06-08 15:23:07 -0600

    changed shebang

--- Jenkisfile_jmeter_test.groovy
+++ Jenkisfile_jmeter_test.groovy
@@ -16,7 +16,7 @@ pipeline {
     stages {
         stage('Starting task') {
             steps {
-                bash
+                sh
 				'''
 				 timeout 60 /opt/jmeter/bin/jmeter.sh -n -t /opt/jmeter/jmeter_kafka_files/TH_APP_th-cef_1.7_9092_3.2_GZIP.jmx
 				'''
